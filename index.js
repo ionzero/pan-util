07011641cbe7fb73e24164263ac56df5d6453ceb
Jay Kuri <jayk@ionzero.com>
2026-01-12 12:57:27 -0700

    Update import path to be correct

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-export * from 'src/constants.js';
-export * from 'src/validators.js';
-export * from 'src/transportEncoding.js';
+export * from './src/constants.js';
+export * from './src/validators.js';
+export * from './src/transportEncoding.js';
